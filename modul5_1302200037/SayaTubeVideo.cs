27474e92d6432a8c8615711d226df453194613f5
NastmaDzia <100849580+NastmaDzia@users.noreply.github.com>
2022-03-26 16:39:10 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- modul5_1302200037/SayaTubeVideo.cs
+++ modul5_1302200037/SayaTubeVideo.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace modul5_1302200037
 {
-    internal class SayaTubeVideo
+    public class SayaTubeVideo
     {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo() { }
+
+        public SayaTubeVideo(string title)
+        {
+            Contract.Requires(title != null);
+            Contract.Requires(title.Length <= 200);
+            this.title = title;
+
+            var rand = new Random();
+            this.id = rand.Next(10000, 99999);
+
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int a)
+        {
+            Contract.Requires(a <= 25000000);
+            try
+            {
+                if (a > 25000000) throw new Exception("Jumlah input play count melebihi 25.000.000!");
+                this.playCount = checked(this.playCount + a);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID\t: {0}", this.id);
+            Console.WriteLine("Video Title\t: {0}", this.title);
+            Console.WriteLine("Video Playcount\t: {0}\n", this.playCount);
+
+        }
     }
 }
